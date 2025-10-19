59eb1226609717ac214549e975d823bdf9d53ae6
MAOAPIM <mortiza2302@alumno.ipn.mx>
2025-10-18 19:56:09 -0600

    Se conecto el js con el HTML para el ejercicio de vectores

--- 06Algoritmia/js/algoritmia.js
+++ 06Algoritmia/js/algoritmia.js
@@ -14,47 +14,38 @@ function problema1() {
     const resultado = palabrasInvertidas.join(' ');
     document.getElementById('p1-output').textContent = resultado;
 }
-
-
-
-
-
-
-
+////////////////////////////////////////////////////////
 function problema2() {
 
-var p2_x1 = document.querySelector('#p2-x1').value;
-var p2_x2 = document.querySelector('#p2-x2').value;
-var p2_x3 = document.querySelector('#p2-x3').value;
-var p2_x4 = document.querySelector('#p2-x4').value;
-var p2_x5 = document.querySelector('#p2-x5').value;
+    var x1 = document.querySelector("#p2-x1").value;
+    var x2 = document.querySelector("#p2-x2").value;
+    var x3 = document.querySelector("#p2-x3").value;
+    var x4 = document.querySelector("#p2-x4").value;
+    var x5 = document.querySelector("#p2-x5").value;
 
-var p2_y1 = document.querySelector('#p2-y1').value;
-var p2_y2 = document.querySelector('#p2-y2').value;
-var p2_y3 = document.querySelector('#p2-y3').value;
-var p2_y4 = document.querySelector('#p2-y4').value;
-var p2_y5 = document.querySelector('#p2-y5').value;
+    var y1 = document.querySelector("#p2-y1").value;
+    var y2 = document.querySelector("#p2-y2").value;
+    var y3 = document.querySelector("#p2-y3").value;
+    var y4 = document.querySelector("#p2-y4").value;
+    var y5 = document.querySelector("#p2-y5").value;
 
-//creamos los vectores
-var v1 = [p2_x1, p2_x2, p2_x3, p2_x4, p2_x5];
-var v2 = [p2_y1, p2_y2, p2_y3, p2_y4, p2_y5];
+    var v1 = [x1, x2, x3, x4, x5];
+    var v2 = [y1, y2, y3, y4, y5];
 
-v1 = v1.sort(function(a, b){return a-b});
-v2 = v2.sort(function(a, b){return b-a});
+    v1 = v1.sort(function (a, b) { return a - b });
+    v2 = v2.sort(function (a, b) { return b - a });
 
-v2 = v2.reverse();
+    v2 = v2.reverse();
 
-var p2_producto = 0;
-for (var i = 0; i < v1.length; i++) {
-    p2_producto += v1[i] * v2[i];
+    var producto = 0;
+
+    for (var i = 0; i < v1.length; i++) {
+        producto = v1[i] * v2[i];
+    }
+
+    document.querySelector("#p2-output").textContent = "El producto escalar minimo es: " + producto;
 }
-
-document.querySelector('#p2-output').textContent = 'El producto escalar minimo es: ' + p2_producto;
-
-
-}
-
-
+////////////////////////////////////////////////////////
 function problema3() {
     const input = document.getElementById('p3-input').value;
     if (input.trim() === '') {
