5e3008d4b3e7657e00db655e9b4c6b776dba3fda
MAOAPIM <mortiza2302@alumno.ipn.mx>
2025-10-22 15:38:54 -0600

    cambios echos en clase

--- 08Rompecabezas/Js/juego.js
+++ 08Rompecabezas/Js/juego.js
@@ -77,10 +77,105 @@ function intercambiarPosicionesRompe(filaPos1, columnaPos1, filaPos2, columnaPos
     rompe[filaPos1, columnaPos2] = pos1;
 }
 
-function iniciar() {
+
+//funcion que se encargue en donde esta la pieza vaicia
+
+function actulizarPosicionVacia(nuevaFila, nuevaColumna){
+    filaVacia = nuevaFila;
+    columnaVaciaVacia = nuevaColumna;
+}
+
+
+function posicionValida(fila, columna){
+    return(fila >= 0 && fila <= 2 && columna >= 0 && columna <= 2);
+}
+
+//De
+
+var codigosDireccion ={
+    IZQUIERDA : 37,
+    ARRIBA : 38,
+    DERECHA : 39,
+    ABAJO : 40
+};
+
+
+function moverEnDireccion (direccion){
+    var nuevaFilaPiezaVacia;
+    var nuevaColumnaPiezaVacia;
+// si se mueve
+
+    if (direccion === codigosDireccion.ABAJO){
+        nuevaFilaPiezaVacia = filaVacia + 1;
+        nuevaColumnaPiezaVacia = columnaVacia;
+    } else if(direccion === codigosDireccion.ARRIBA){
+        nuevaFilaPiezaVacia = filaVacia - 1;
+        nuevaColumnaPiezaVacia = columnaVacia;
+    }else if(direccion === codigosDireccion.DERECHA){
+        nuevaFilaPiezaVacia = filaVacia;
+        nuevaColumnaPiezaVacia = columnaVacia + 1;
+    }else if(direccion === codigosDireccion.IZQUIERDA){
+        nuevaFilaPiezaVacia = filaVacia;
+        nuevaColumnaPiezaVacia = columnaVacia - 1;
+    }
+
+
+    if(posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia))
+    {
+        //Funcin que haga que se intercambir las posiciones
+        intercambiarPosiciones(filaVacia,columnaVacia,nuevaFilaPiezaVacia,nuevaColumnaPiezaVacia);
+        actulizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+    }
+}
+
+
+function intercambiarPosiciones(fila1, columna1, fila2, columna2) {
+    var pos1 = rompe[fila1, columna1];
+    var pos2 = rompe[fila2, columna2];
+    //Intercambio ya debe ser por parte del frame del HTML
+
+    intercambiarPosicionesRompe(fila1, columna1, fila2, columna2);
+    // para HTML
+    intercambiarPosicionesDOM(`pieza`+pieza1, `pieza`+pieza2)
+}
+
+
+
+function intercambiarPosicionesDOM(idPieza1, idPieza2){
+    var pieza1=document.getElementById(idPieza1);
+    var pieza2=document.getElementById(idPieza2);
+
+
+    //Vamos a clonar
+
+    var padre = Elementpieza1.parentNode;
+
+
+    var cloneElemento1 = pieza1.cloneNode(true);
+    var cloneElemento2 = pieza2.cloneNode(false);
+
+///Para este punto no funciona el apartado de la varaibla padre por alguna extraña razon 
+
+
+// COPIAR DE ALGUN REPO  PARA GUIARNOS 
+
+
+
+
+}
+
+//Sola mando a llamar a que la posicion sea valida
+
+function iniciar () {
     //Mezclar las piezas
+    mezclarPiezas(30);
+    capturarTeclas();
     //Capturar el último movimiento
 }
 
+iniciar();
+
 //Mandamos traer a la función
 mostrarInstrucciones(instrucciones);
+
+
